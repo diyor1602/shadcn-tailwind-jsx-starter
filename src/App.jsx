df6f2d5a9407ad3abf32cbf96c3ae1e64c4b2bff
diyor1602 <abdurakhmanov1602@gmail.com>
2024-10-31 09:11:47 +0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="text-center p-10">
+      Start coding here!
+      <h1 className="text-red-700"> Hello World </h1>
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default App;
